a44bb79a31f266dc1ad89ae23a0de5ddfc24a7ee
Elliot Block <elliot@deck36.net>
2014-07-08 12:03:25 -0700

    Decompose dpkg functionality in apt module
    By breaking apart the functions we can use some of the dpkg logic
    so that it works either locally or remotely.  Once you've gotten
    the dpkg output (either locally from subprocess, or remotely over
    SSH), the same exact functions can parse it etc.
    
    This will help us build the mcv.remote.apt module in a forthcoming
    commit.

--- mcv/apt.py
+++ mcv/apt.py
@@ -5,19 +5,41 @@ import os
 
 apt_cmd = "/usr/bin/apt-get"
 
-def _status(pkg):
-    cmd = ["/usr/bin/dpkg-query", "-W", "-f", "${Status}", pkg]
+# Some of the dpkg facilities are factored out so that they're usable
+# both locally here and remotely by mcv.remote.apt
+def _dpkg_query_cmd(pkg):
+    return ["/usr/bin/dpkg-query", "-W", "-f", "'${Status}'", pkg]
+
+def _dpkg_query_local(pkg):
+    cmd = _dpkg_query_cmd(pkg)
     try:
         with open(os.devnull, 'w') as devnull:
             output = subprocess.check_output(cmd, stderr=devnull)
-        fields = output.split(' ')
-        status = fields[2]
-        return status
+        return output
     except subprocess.CalledProcessError, e:
         return None
 
+def _dpkg_status(dpkg_query_output):
+    """Return the status of a package given its dpkg query output
+
+    This is split out into separate components so that it's
+    agnostic as to whether the dpkg output came from a local
+    source or a remote one, e.g. see mcv.remote.apt"""
+    normalized = dpkg_query_output.strip()
+    if not normalized:
+        return None
+
+    fields = normalized.split(' ')
+    status = fields[2]
+
+    if status == 'not-installed':
+        return None
+    else:
+        return status
+
 def status(pkgs):
-    return { p:_status(p) for p in pkgs }
+    """Return the install status of the given packages."""
+    return { p:_dpkg_status(_dpkg_query_local(p)) for p in pkgs }
 
 def _install(pkgs):
     if not pkgs:
