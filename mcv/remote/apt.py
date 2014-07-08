0846ea0903a48f5f806ee173439d2fe768826804
Elliot Block <elliot@deck36.net>
2014-07-08 12:05:26 -0700

    Add mcv.remote.apt
    This commit adds mcv.remote.apt, which allows us to install APT
    packages remotely over SSH.  It composes the standard mcv.remote
    functions with the mcv.apt functions to do APT work remotely.

---
+++ mcv/remote/apt.py
@@ -0,0 +1,29 @@
+import mcv.remote
+import mcv.apt
+
+import sys
+
+def _status(ssh, pkg, verbose=False):
+    raw_out, err = mcv.remote.execute(ssh, mcv.apt._dpkg_query_cmd(pkg))
+    out = raw_out + "\n" # dpkg output by default has no newline
+    if verbose:
+        sys.stdout.write(out)
+        sys.stderr.write(err)
+    return mcv.apt._dpkg_status(out)
+
+def status(ssh, pkgs, verbose=False):
+    return { p:_status(ssh, p, verbose=verbose) for p in pkgs }
+
+def _install(ssh, pkgs):
+    if not pkgs:
+        return (None, None, None)
+
+    out, err = mcv.remote.execute(ssh, [mcv.apt.apt_cmd, 'install', '-y'] + pkgs, sudo=True)
+    return (True, out, err)
+
+def install(ssh, pkgs, verbose=False):
+    installed_packages = status(ssh, pkgs)
+    pkgs_to_install = [p for p in pkgs if not installed_packages[p]]
+    install_status, out, err = _install(ssh, pkgs_to_install)
+    return (install_status, out, err)
+
