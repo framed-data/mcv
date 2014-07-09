c66d3f2399918d9cd7a41b2446c8da91749c0303
Elliot Block <elliot@deck36.net>
2014-07-09 12:51:18 -0700

    Include default port number by default

--- mcv/remote/__init__.py
+++ mcv/remote/__init__.py
@@ -12,6 +12,7 @@ from contextlib import contextmanager
 def conn_spec():
     """Return a connection specification with sane default values."""
     return {'username': getpass.getuser(),
+            'port': 22,
             'missing_host_key_policy': paramiko.AutoAddPolicy(),
             'host_keys_path': os.path.join("~", ".ssh", "known_hosts")}
 
