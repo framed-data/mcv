b6893b3a56827b07581dd904e55e8bc0386b083f
Elliot Block <elliot@deck36.net>
2014-07-17 13:27:17 -0700

    Bump minor version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "mcv",
-    version = "0.8.0",
+    version = "0.9.0",
     packages = find_packages(),
     install_requires = [
         'pyyaml',
