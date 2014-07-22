74c55ef9668ad819e7e62a33322132752239add4
Elliot Block <elliot@deck36.net>
2014-07-22 11:12:38 -0700

    Add basic Upstart starter
    This commit gives basic functions to query status of Upstart
    service and idempotently start/restart services.

---
+++ mcv/upstart.py
@@ -0,0 +1,25 @@
+import subprocess
+import re
+
+def status(service_name):
+    """Query Upstart status"""
+    output = subprocess.check_output(['status', service_name])
+    r = re.compile('(?P<service>\S+) (?P<status>[\w/]+)(, process (?P<proc>\d+))?')
+    m = r.match(output)
+    service, status, _, pid = m.groups()
+    return [service, status, pid]
+
+def start(service_name, restart=False):
+    """Idempotent Upstart start"""
+    _, status, _ = status(service_name)
+
+    action = None
+    if status == 'start/running' and restart:
+        action = 'restart'
+    else:
+        action = 'start'
+
+    if action:
+        return subprocess.call([action, service_name])
+    else:
+        return None
