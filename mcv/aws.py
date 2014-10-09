27ff416462e1e15264fb529f0a33413cffc8281a
Jeff Terrell <jeff.terrell@acm.org>
2014-10-09 20:21:17 +0000

    Conform ' vs. " strings to Python conventions

--- mcv/aws.py
+++ mcv/aws.py
@@ -1,10 +1,10 @@
-'''AWS utilities'''
+"""AWS utilities"""
 
 import subprocess
 import os
 import json
 
-aws_cmd = '/usr/local/bin/aws'
+aws_cmd = "/usr/local/bin/aws"
 
 def my_assert(cond, message):
     if not cond:
@@ -12,12 +12,12 @@ def my_assert(cond, message):
         os._exit(1)
 
 def has_key_pair(key_pair):
-    with open('/dev/null', 'w') as devnull:
-        cmd = [aws_cmd, 'ec2', 'describe-key-pairs', '--key-names', key_pair]
+    with open("/dev/null", 'w') as devnull:
+        cmd = [aws_cmd, "ec2", "describe-key-pairs", "--key-names", key_pair]
         return subprocess.call(cmd, stdout=devnull, stderr=devnull) == 0
 
 def has_bucket(bucket):
-    with open('/dev/null', 'w') as devnull:
+    with open("/dev/null", 'w') as devnull:
         cmd = [aws_cmd, 's3api', 'list-buckets']
         output = subprocess.check_output(cmd, stderr=devnull)
     buckets = json.loads(output)['Buckets']
@@ -25,29 +25,29 @@ def has_bucket(bucket):
     return bucket in bkt_names
 
 def has_s3_object(bucket, path):
-    s3_obj = 's3://%s/%s' % (bucket, path.lstrip('/'))
-    with open('/dev/null', 'w') as devnull:
+    s3_obj = "s3://%s/%s" % (bucket, path.lstrip("/"))
+    with open("/dev/null", 'w') as devnull:
         cmd = [aws_cmd, 's3', 'ls', s3_obj]
         output = subprocess.check_output(cmd, stderr=devnull)
     return len(output) > 0
 
 def assert_key_pair(key_pair):
-    my_assert(has_key_pair(key_pair), '''
+    my_assert(has_key_pair(key_pair), """
 Error: unable to find key-pair '%s'.
 Perhaps the key-pair does not exist, or you do not have access to it?
 (You can check this with `aws ec2 describe-key-pairs`.)
-'''.strip() % key_pair)
+""".strip() % key_pair)
 
 def assert_bucket(bucket):
-    my_assert(has_bucket(bucket), '''
+    my_assert(has_bucket(bucket), """
 Error: unable to find bucket '%s'.
 Perhaps the bucket does not exist, or you do not have access to it?
 (You can check this with `aws s3api list-buckets`.)
-'''.strip() % bucket)
+""".strip() % bucket)
 
 def assert_s3_object(bucket, path):
-    my_assert(has_s3_object(bucket, path), '''
+    my_assert(has_s3_object(bucket, path), """
 Error: unable to find S3 object '%s' in bucket '%s'.
 Perhaps the bucket or file does not exist, or you do not have access to it?
 (You can check this with `aws s3api list-buckets` and `aws s3 ls S3_PATH`.)
-'''.strip() % (path, bucket))
+""".strip() % (path, bucket))
