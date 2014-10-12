3a8d8ab24aab5518035fbe8d460d206e0410fd78
Elliot Block <elliot@framed.io>
2014-10-12 00:57:13 +0000

    Fix PEP8 violations in dir.py

--- mcv/dir.py
+++ mcv/dir.py
@@ -12,7 +12,9 @@ level  if necessary.
 
 import treant
 import os
-import mcv.file, mcv.util
+import mcv.file
+import mcv.util
+
 
 def tree(node):
     """Construct a tree from nodes; use the treant 'smart' node
@@ -26,6 +28,7 @@ def tree(node):
     """
     return treant.tree(node, node_constructor=treant.n)
 
+
 def _path(tree_path):
     """Given a tree path returns its directory path.
 
@@ -36,6 +39,7 @@ def _path(tree_path):
     # to their values, e.g. ["/", "home", "john"]
     tpath_values = [treant.value(node) for node in tree_path]
     return os.path.join(*tpath_values)
+
 
 def _opts(tree_path):
     """Given a tree path return its computed opts.  Opts
@@ -52,9 +56,11 @@ def _opts(tree_path):
     tpath_opts = [treant.attrs(node) for node in tree_path]
     return mcv.util.merge_dicts(*tpath_opts)
 
+
 def root_path(tree):
     """Returns the root path of a tree, assumed to just be its value."""
     return treant.value(tree)
+
 
 def subpath(tree, search_name, *subpaths):
     """Find a node in the tree and return a subpath thereof.
@@ -75,17 +81,20 @@ def subpath(tree, search_name, *subpaths):
         # => None #; if the search fails, don't subpath.
     """
     # get the path values themselves out of the tpath, not the full nodes
-    root_tpath = treant.find_path_ex(tree, lambda n: treant.value(n) == search_name)
+    root_tpath = treant.find_path_ex(tree,
+                                     lambda n: treant.value(n) == search_name)
 
     if not root_tpath:
         return None
 
     return os.path.join(*[_path(root_tpath)] + [p for p in subpaths])
 
+
 def paths(tree):
     # list of tpaths, i.e. list of (list of nodes)
     tps = [tp for tp in treant.paths_preorder(tree)]
     return [_path(tp) for tp in tps]
+
 
 def _mktree(tree):
     """Given a tree, return a list of (path, opts) tuples
@@ -98,6 +107,7 @@ def _mktree(tree):
     opts = [_opts(tp) for tp in tps]
 
     return zip(paths, opts)
+
 
 def mktree(tree, pred=lambda opts: True):
     """Given a tree, make all the directories in the tree
