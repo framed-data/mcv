a39ecdb419357820951d5855f72199dab3cfa7eb
Elliot Block <elliot@framed.io>
2014-10-12 01:09:46 +0000

    Fix PEP8 violations in test_dir.py

--- tests/test_dir.py
+++ tests/test_dir.py
@@ -5,12 +5,12 @@ from nose.tools import eq_
 
 tree0 = mcv.dir.tree(
     ('/', [
-         ('opt', [
-             ('framed', [
-                 ('bin')]),
-             ('mcv', [
-                 ('bin'),
-                 ('tests')])])]))
+        ('opt', [
+            ('framed', [
+                ('bin')]),
+            ('mcv', [
+                ('bin'),
+                ('tests')])])]))
 
 framed_attrs = {
     'group': 'framed',
@@ -19,19 +19,22 @@ framed_attrs = {
 
 tree1 = mcv.dir.tree(
     ('/', [
-         ('opt', [
-             ('framed', framed_attrs, [
-                 ('bin'),
-                 ('public', { 'mode': 0777 })]),
-             ('mcv', framed_attrs, [
-                 ('bin') ])])]))
+        ('opt', [
+            ('framed', framed_attrs, [
+                ('bin'),
+                ('public', {'mode': 0777})]),
+            ('mcv', framed_attrs, [
+                ('bin')])])]))
+
 
 def test_subpath_hit():
     eq_(mcv.dir.subpath(tree0, 'framed', 'src'),
         "/opt/framed/src")
 
+
 def test_subpath_miss():
     eq_(mcv.dir.subpath(tree0, 'nonsense!', 'src'), None)
+
 
 def test_paths():
     eq_(mcv.dir.paths(tree0),
@@ -43,13 +46,13 @@ def test_paths():
          '/opt/mcv/bin',
          '/opt/mcv/tests'])
 
+
 def test__mktree():
     eq_(mcv.dir._mktree(tree1),
         [("/", {}),
          ("/opt", {}),
          ("/opt/framed", framed_attrs),
          ("/opt/framed/bin", framed_attrs),
-         ("/opt/framed/public", { 'group': 'framed', 'mode': 0777 }),
+         ("/opt/framed/public", {'group': 'framed', 'mode': 0777}),
          ("/opt/mcv", framed_attrs),
          ("/opt/mcv/bin", framed_attrs)])
-
