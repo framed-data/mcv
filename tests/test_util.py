6ba0a274183dd07dae22d7fc6db24d1d21ae909a
Elliot Block <elliot@framed.io>
2014-10-12 01:10:56 +0000

    Fix PEP8 violations in test_util.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,24 +1,27 @@
 import mcv.util
 from nose.tools import eq_
 
+
 def test_select_keys_hit():
-    d = { 'foo': 20, 'bar': 40 }
+    d = {'foo': 20, 'bar': 40}
 
     eq_(mcv.util.select_keys(d, ['foo']),
         {'foo': 20})
 
+
 def test_select_keys_miss():
-    d = { 'foo': 20, 'bar': 40 }
+    d = {'foo': 20, 'bar': 40}
 
     eq_(mcv.util.select_keys(d, ['foo', 'baz']),
         {'foo': 20})
 
+
 def test_merge_dicts():
-    d0 = { 'foo': 20, 'bar': 40 }
-    d1 = { 'baz': 60 }
-    d2 = { 'foo': 30 }
+    d0 = {'foo': 20, 'bar': 40}
+    d1 = {'baz': 60}
+    d2 = {'foo': 30}
 
     eq_(mcv.util.merge_dicts(d0, d1, d2),
-        { 'foo': 30,
-          'bar': 40,
-          'baz': 60 })
+        {'foo': 30,
+         'bar': 40,
+         'baz': 60})
