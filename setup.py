b26dfe858ae1423f825676481d766ca82d94c2c2
Andrew Berls <andrew.berls@gmail.com>
2015-01-31 22:19:26 +0000

    Cut mcv 0.25.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "mcv",
-    version = "0.24.0",
+    version = "0.25.0",
     packages = find_packages(),
     install_requires = [
         'pyyaml',
