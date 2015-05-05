283e4e4a3e029ddeecc179eab6e0a0219409f34f
Andrew Berls <andrew.berls@gmail.com>
2015-05-05 20:35:59 +0000

    nginx: Use opts dict instead of addrs/listen

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "mcv",
-    version = "0.27.0",
+    version = "0.28.0",
     packages = find_packages(),
     install_requires = [
         'pyyaml',
