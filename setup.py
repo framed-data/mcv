fbf8679716a65b30f9c9c467de3bd8c29fd03eca
Andrew Berls <andrew.berls@gmail.com>
2015-09-27 12:02:11 +0000

    Add mcv.hostname
    Add a utility to set the system hostname, in a manner that should
    survive restarts and modifies /etc/hosts to prevent unknown host
    warnings

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "mcv",
-    version = "0.29.0",
+    version = "0.30.0",
     packages = find_packages(),
     install_requires = [
         'pyyaml',
