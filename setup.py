aa8467482c278a60d53d796ae214ba039dc53a55
Elliot Block <elliot@framed.io>
2016-01-30 21:09:26 +0000

    Bump patch version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "mcv",
-    version = "0.31.0",
+    version = "0.31.1",
     packages = find_packages(),
     install_requires = [
         'pyyaml',
